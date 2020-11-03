c1ef5d6970e0f64d1ac49e5cc720737b20fc67b1
Rafael 99 <chipalino2009@list.ru>
2020-11-03 21:48:52 +0300

    Update site design

---
+++ src/components/Feature/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {FeatureContainer, FeatureButtom } from './FeatureElements';
+
+const Feature = () => {
+    return (
+        <FeatureContainer>
+            <h1>Pizza of the day</h1>
+            <p>Truffle alfredo sauce topped with 24 carat gold dust.</p>
+            <FeatureButtom>Order Now!</FeatureButtom>
+        </FeatureContainer>
+    )
+}
+
+export default Feature
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import {
+    SidebarContainer, 
+    Icon, 
+    CloseIcon, 
+    SidebarMenu, 
+    SidebarLink, 
+    SideBtnWrap, 
+    SidebarRoute,
+} from './SidebarElements'
+const Sidebar = ({isOpen, toggle}) => {
+  return (
+    <SidebarContainer isOpen={isOpen} onClick={toggle}>
+        <Icon>
+          <CloseIcon/>
+        </Icon>
+        <SidebarMenu>
+            <SidebarLink to="/">Pizzas</SidebarLink>
+            <SidebarLink to="/">Desserts</SidebarLink>
+            <SidebarLink to="/">Full menu</SidebarLink>
+        </SidebarMenu>
+        <SideBtnWrap>
+        <SidebarRoute to="/">Order now</SidebarRoute>
+        </SideBtnWrap>
+    </SidebarContainer>
+    )
+}
+
+export default Sidebar
